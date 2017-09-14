cb5376aa88d777309ac3fbf88af8facd78800a7e
Peter Jaeger <peter@jaeger.to>
2017-09-14 11:10:50 +0200

    v.001

---
+++ HalloAlexa/Controllers/AlexaController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http;
+
+namespace HalloAlexa.Controllers
+{
+    public class AlexaController : ApiController
+    {
+        [HttpPost, Route("api/alexa/demo")]
+        public dynamic HelloWorld(dynamic request)
+        {
+            return new
+            {
+                version = "1.0",
+                sessionAttributes = new { },
+                response = new
+                {
+                    outputSpeech = new
+                    {
+                        type = "PlainText",
+                        text = "Hello World"
+                    },
+                    card = new
+                    {
+                        type = "Simple",
+                        title = "HelloWorld",
+                        content = "Hello fine new Voice world !"
+                    },
+                    shouldEndSession = true
+                }
+            };
+        }
+    }
+}
